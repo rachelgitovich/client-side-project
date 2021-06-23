92c6fd4be2e88f17730f2a74696591c369759cfc
Rachel Gitovich <rachelibr050@gmail.com>
2021-06-23 21:05:41 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import About from './components/About';
+import Home from './components/Home';
+import Products from './components/Products';
+import Product from './components/Product';
+import Checkout from './components/Checkout';
+import Final from './components/Final';
+import store from './redux/store';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Provider store={store}>
+			<Router>
+				
+				<Switch>
+					<Route path='/about'>
+						<About>
+							<h1>History of the store</h1>
+							<h1>Reasons for opening the store</h1>
+							<h1>The current status of the store</h1>
+						</About>
+					</Route>
+					<Route path='/product' component={Product} />
+					<Route path='/products' component={Products} />
+					<Route path='/checkout' component={Checkout} />
+					<Route path='/final-checkout' component={Final} />
+					<Route path='/' component={Home} />
+				</Switch>
+			</Router>
+		</Provider>
+	);
 }
 
 export default App;
